33858a8c5209fa9dd396c9199fe89eb423999673
Anthony Oteri <anthony.oteri@edgeware.tv>
2017-06-23 23:22:16 -0400

    Add config module

---
+++ cronos/config.py
@@ -0,0 +1,35 @@
+# Copyright (C) 2017, Anthony Oteri
+# All rights reserved.
+"""Module for holding onto the configuration."""
+
+from __future__ import absolute_import
+
+import logging
+import json
+import yaml
+
+log = logging.getLogger(__name__)
+
+config = {}
+
+
+def load(filename, dump=None):
+    """Load the configuration from the given filename.
+
+    If `dump` is truthful, dump the config to the logfile.
+    """
+    global config
+    try:
+        with open(filename, 'r') as config_file:
+            config = yaml.load(config_file)
+        if dump:
+            log.debug(json.dumps(config, indent=2))
+
+    except IOError as e:
+        log.error("Error reading configuration file %s: %s", filename, e)
+
+
+def get(key, *default):
+    """Get a value from the configuration."""
+
+    return config.get(key, *default)
