0ba788d42e04e57b37c0c43a55838c4965854a37
Anthony Oteri <anthony.oteri@edgeware.tv>
2017-06-27 11:07:24 -0400

    Refactoring

--- cronos/ui/__init__.py
+++ cronos/ui/__init__.py
@@ -4,11 +4,8 @@
 """User Interface modules."""
 
 from cronos.ui.clock import Clock
-from cronos.ui.ledger import Ledger
 from cronos.ui.log import Log
 from cronos.ui.project import Project
-from cronos.ui.today import Month
-from cronos.ui.today import Today
-from cronos.ui.today import Week
+from cronos.ui.reporting import Ledger, Month, Today, Week
 
 __all__ = [Clock, Ledger, Log, Month, Project, Today, Week]
--- cronos/ui/reporting.py
+++ cronos/ui/reporting.py
@@ -1,24 +1,162 @@
-# Copyright (C) 2018, Anthony Oteri
-# All rights reserved.
-"""UI widget for displaying the daily, weekly, and monthly reports."""
+# Copyright (C) 2017, Anthony Oteri.
+# All rights reserved
+"""Widgets for reporting time."""
 
 from __future__ import absolute_import
 
 import collections
-from datetime import datetime, timedelta
 import logging
 import time
 import Tkinter as tk
-
+import ttk
+from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta, MO, SU
 
 from cronos import event
 from cronos.db import RecordService
-from cronos.ui.console import Console
-from cronos.utils import human_time
+from cronos.utils import human_time, local_time
 
 
 log = logging.getLogger(__name__)
+
+
+class Console(ttk.Frame, object):
+    """Widget for displaying text in a console."""
+
+    POLLING_INTERVAL_MS = 250
+
+    def __init__(self, master):
+        """Construct the layout and initialize internal state."""
+        ttk.Frame.__init__(self, master)
+
+        self._text = ''
+
+        # Allow subclasses to override the polling interval.
+        self.polling_interval_ms = Console.POLLING_INTERVAL_MS
+
+        self.configure_layout()
+        self.create_widgets()
+        self._poll()
+
+    def configure_layout(self):
+        """Configure the grid layout."""
+        for row in xrange(50):
+            self.rowconfigure(row, weight=1)
+
+        for col in xrange(24):
+            self.rowconfigure(col, weight=1)
+
+    def create_widgets(self):
+        """Layout the elements on screen."""
+
+        self.box = tk.Text(self)
+        self.box.grid(row=0, column=0, rowspan=50, columnspan=24,
+                      sticky='news')
+
+    @property
+    def text(self):
+        return self._text
+
+    @text.setter
+    def text(self, value):
+        """Set the text to be displayed in the window."""
+        self._text = str(value)
+
+    def _update(self):
+        """Update the displayed contents of the window."""
+
+        self.box['state'] = tk.NORMAL
+        self.box.delete(1.0, tk.END)
+
+        for line in self._text.splitlines():
+            self.box.insert(tk.END, line + "\n")
+
+        # Move the cursor to the end to prevent the display from jumping
+        # back to the beginning after each refresh.
+        self.box.see(tk.END)
+
+        self.box['state'] = tk.DISABLED
+
+    def _poll(self):
+        """Repeatedly call the `self.poll()` method."""
+
+        self.poll()
+        self.after(self.polling_interval_ms, self._poll)
+
+    def poll(self):
+        """Method to perform the actual polling work.
+
+        Subclasses should override this method to contain whatever work needs
+        to be performed periodically.  Adjust the `self.polling_interval_ms`
+        variable to control how often this code is executed.
+        """
+        pass
+
+
+class Ledger(Console):
+    """Widget for displaying a report of all records per day."""
+
+    def __init__(self, master):
+        """Construct layout and internal state."""
+
+        super(Ledger, self).__init__(master)
+
+        # A dictionary of day to entries.
+        self.data = collections.defaultdict(list)
+
+        self.record_service = RecordService()
+
+        self.polling_interval_ms = 1000
+
+        event.register(self.update)
+
+    def load(self):
+        """Load the records from the database."""
+
+        self.data = self.record_service.by_day()
+
+    def update(self):
+        """Refresh the output of the report"""
+        self.load()
+
+        self.text = '\n'.join(self._make_report())
+
+        # Be sure to redraw the text box.
+        self._update()
+
+    def _make_report(self):
+        """Generates a report.
+
+        The report will have the following format for each day:
+
+            2017-06-23
+              PROJECT         |    START |     STOP | ELAPSED
+              foo             | 19:03:27 | 21:05:00 | 01:02
+              bar             | 21:06:00 | 21:36:00 | 00:30
+              bar             | 21:36:10 | 21:56:00 | 00:20
+
+        Note: Entries less than 2m will be ommitted.
+        """
+
+        fmt = "  %-25s | %8s | %8s | %s"
+
+        for day, entries in sorted(self.data.iteritems()):
+            yield day
+            yield fmt % ("PROJECT", "START", "STOP", "ELAPSED")
+
+            for entry in entries:
+
+                # Skip entries which are insignificant.
+                if entry['stop_ts'] - entry['start_ts'] < 120:
+                    continue
+
+                yield fmt % (
+                    entry['project'],
+                    local_time(datetime.utcfromtimestamp(
+                        entry['start_ts'])).time().isoformat(),
+                    local_time(datetime.utcfromtimestamp(
+                        entry['stop_ts'])).time().isoformat(),
+                    human_time(entry['elapsed'], 0))
 
 
 class Report(Console):
@@ -215,3 +353,4 @@ class Month(Report):
 
     def stop(self):
         return self.end_of_month(self.reference_point)
+
